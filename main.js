10f632a686fb31e01b473b8f3dfdf8f77b44a92f
Pranit2809 <95509557+Pranit2809@users.noreply.github.com>
2022-08-06 17:51:16 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-//Create a reference for canvas 
-
-//Give specific height and width to the car image
+canvas=document.getElementById('myCanvas') ;
+ctx = canvas.getContext("2d")
+greencar__width= 75;
+greencar_height=100;
 
 background_image = "parkingLot.jpg";
 greencar_image = "car2.png";
 
-//Set initial position for a car image.
+greencar_x = 5;
+greencar_y = 225;
 
 function add() {
-	//upload car, and background images on the canvas.
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image;
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadgreencar;
+	greencar_imgTag.src = greencar_image;
 }
 
 function uploadBackground() {
-	//Define function ‘uploadBackground’
+ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 
 }
 
 function uploadgreencar() {
-	//Define function ‘uploadgreencar’.
+ctx.drawImage(greencar_imgTag, greencar_x, greencar_y,greencar__width, greencar_height);
 
 	
 }
@@ -58,20 +66,38 @@ function my_keydown(e)
 
 function up()
 {
-	//Define function to move the car upward
+if(greencar_y >=0){
+	greencar_y = greencar_y - 10;
+	uploadBackground();
+	uploadgreencar();
+}
 }
 
 function down()
 {
-	//Define function to move the car downward
+	if(greencar_y >=0){
+		greencar_y = greencar_y + 10;
+		uploadBackground();
+		uploadgreencar();
+	}
 }
 
 function left()
 {
-	//Define function to move the car left side
+	if(greencar_x >=0){
+		greencar_x = greencar_x - 10;
+		uploadBackground();
+		uploadgreencar();
+	}
 }
 
 function right()
 {
-	//Define function to move the car right side
+	console.log("bananna");
+	if(greencar_x >=0){
+		console.log("bananna");
+		greencar_x = greencar_x + 10;
+		uploadBackground();
+		uploadgreencar();
+	}
 }
